314fcb02bf8bcfe1e99cff4bc42efc65f130bdde
mohammedalhazlool <145846844+mohammedalhazlool@users.noreply.github.com>
2023-10-14 16:30:46 +0300

    Add files via upload

---
+++ contact.java
@@ -0,0 +1,88 @@
+package a;
+
+import java.util.Scanner;
+
+public class contact implements Comparable<String>
+{
+private String name;
+private String number;
+private String birthday;
+private String notes;
+private String email;
+private String address;
+public contact() {
+	
+}
+public contact(String name, String number, String birthday, String notes, String email, String address) {
+	this.name= name;
+	this.number=number;
+	this.birthday=birthday;
+	this.notes=notes;
+	this.email=email;
+	this.address=address;
+}
+
+public contact(contact a) {
+	this.address=a.address;
+	this.email=a.email;
+	this.name=a.name;
+	this.number=a.number;
+	this.birthday=a.birthday;
+	this.notes=a.notes;
+	
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getNumber() {
+	return number;
+}
+public void setNumber(String number) {
+	this.number = number;
+}
+public String getBirthday() {
+	return birthday;
+}
+public void setBirthday(String birthday) {
+	this.birthday = birthday;
+}
+public String getNotes() {
+	return notes;
+}
+public void setNotes(String notes) {
+	this.notes = notes;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+
+@Override
+public int compareTo(String n) {
+	// TODO Auto-generated method stub
+	return (name.compareTo(n));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
