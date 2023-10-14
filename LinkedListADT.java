b870d53b433b8e18e3a6c2811b902fb5fac7d2bd
MuhannadAlasmari <147306826+MuhannadAlasmari@users.noreply.github.com>
2023-10-14 17:36:51 +0300

    Update LinkedListADT.java

--- LinkedListADT.java
+++ LinkedListADT.java
@@ -1,3 +1,14 @@
+/***********************************
+CLASS: LinkedListADT.java
+CSC212 Data structures - Project phase I
+Fall 2023
+EDIT DATE:
+13-10-2023
+TEAM:
+A+
+AUTHORS:
+Muhannad Alasmari , 443102430
+***********************************/
 public class LinkedListADT<T>{
 	private Node<T> head;
 	private Node<T> current;
