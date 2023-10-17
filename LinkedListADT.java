3efd3509971b895982bac58610f36dfeb11bd7d5
MuhannadAlasmari <147306826+MuhannadAlasmari@users.noreply.github.com>
2023-10-17 17:00:07 +0300

    Update LinkedListADT.java

--- LinkedListADT.java
+++ LinkedListADT.java
@@ -9,6 +9,38 @@ MMA
 AUTHORS:
 Muhannad Alasmari , 443102430
 ***********************************/
+class Node <T>{
+	public T data;
+	public Node<T> next;
+	
+
+	public Node () {
+		data = null;
+		next = null;
+	}
+
+	public Node (T val) {
+		data = val;
+		next = null;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public Node<T> getNext() {
+		return next;
+	}
+
+}
 public class LinkedListADT<T>{
 	private Node<T> head;
 	private Node<T> current;
